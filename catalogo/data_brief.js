c7e444974f5ef4a6fe429210913ea29b4635afee
cwulfman <cwulfman@princeton.edu>
2023-01-12 11:00:02 -0500

    adds data js files

---
+++ catalogo/data_brief.js
@@ -0,0 +1,65 @@
+const data = [
+              {
+                  "id": "http://portal.getty.edu/books/gri_9921990690001551",
+                  "cicognaraNumber": "791A",
+                  "dclNumber": "f2w",
+                  "creator": [
+                      "Lipsius, Justus, 1547-1606"
+                  ],
+                  "description": [
+                      "Printed by Plantin in Leiden with the Antwerp imprint.",
+                      "Signatures: A-N4 (last blank).",
+                      "Engravings have been attributed to Pieter van der Borcht."
+                  ],
+                  "issued": [
+                      "[M].[D].LXXXIV [1584]",
+                      "1584"
+                  ],
+                  "language": [
+                      "Latin"
+                  ],
+                  "title": [
+                      "Ivsti LipsI de amphitheatro liber : in quo forma ipsa loci expressa, & ration spectandi / cum æneis figuris."
+                  ],
+                  "publisher": [
+                      "Antverpiæ : Apud Christophorum Plantinum"
+                  ],
+                  "subject": [
+                      "Amphitheaters.",
+                      "Amphitheaters -- Italy.",
+                      "Amphitheaters",
+                      "Italy."
+                  ]
+              },
+              {
+                  "id": "http://portal.getty.edu/books/gri_9921990690001551",
+                  "cicognaraNumber": "791A",
+                  "dclNumber": "f2w",
+                  "creator": [
+                      "Lipsius, Justus, 1547-1606"
+                  ],
+                  "description": [
+                      "Printed by Plantin in Leiden with the Antwerp imprint.",
+                      "Signatures: A-N4 (last blank).",
+                      "Engravings have been attributed to Pieter van der Borcht."
+                  ],
+                  "issued": [
+                      "[M].[D].LXXXIV [1584]",
+                      "1584"
+                  ],
+                  "language": [
+                      "Latin"
+                  ],
+                  "title": [
+                      "Ivsti LipsI de amphitheatro liber : in quo forma ipsa loci expressa, & ration spectandi / cum æneis figuris."
+                  ],
+                  "publisher": [
+                      "Antverpiæ : Apud Christophorum Plantinum"
+                  ],
+                  "subject": [
+                      "Amphitheaters.",
+                      "Amphitheaters -- Italy.",
+                      "Amphitheaters",
+                      "Italy."
+                  ]
+              }];
