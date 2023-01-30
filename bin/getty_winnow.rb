8d539f7c91537fbc5281e44bc696ad3ebed98585
cwulfman <cwulfman@princeton.edu>
2023-01-30 13:21:30 -0500

    corrects getty_winnow algorithm

--- bin/getty_winnow.rb
+++ bin/getty_winnow.rb
@@ -17,13 +17,15 @@ FileUtils.mkdir_p outdir
 Dir["#{basedir}/tmp/resources/*.xml"].each do |file|
   record = Nokogiri::XML(File.read(file))
 
-  copyit = false
-
   ids = record.xpath('//dc:identifier')
 
   has_ids = ids.any? {|id| ['cico', 'dcl'].include? id.text.split(':').first }
 
   has_manifest = record.xpath('//dcterms:hasFormat').text.split('/').last == 'manifest'
 
-  FileUtils.cp file, outdir if has_ids and has_manifest
+  collections = record.xpath('//dcterms:isPartOf')
+  in_collection = collections.any?{ |c| c.text == "Cicognara Collection"}
+
+  # puts "incollection: |#{collections}|" if in_collection
+  FileUtils.cp file, outdir if has_ids and in_collection
 end
