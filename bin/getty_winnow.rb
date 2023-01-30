4cd9c2a9b4d10fadb3981747284f5d84321b3516
cwulfman <cwulfman@princeton.edu>
2023-01-30 13:22:53 -0500

    Code cleanup

--- bin/getty_winnow.rb
+++ bin/getty_winnow.rb
@@ -5,11 +5,8 @@ require "logger"
 require "nokogiri"
 
 basedir = "#{File.dirname(__FILE__)}/.."
-
 logger = Logger.new(STDOUT)
-
 records = []
-
 outdir =basedir + '/tmp/cicorecords'
 
 FileUtils.mkdir_p outdir
@@ -18,14 +15,10 @@ Dir["#{basedir}/tmp/resources/*.xml"].each do |file|
   record = Nokogiri::XML(File.read(file))
 
   ids = record.xpath('//dc:identifier')
-
   has_ids = ids.any? {|id| ['cico', 'dcl'].include? id.text.split(':').first }
-
-  has_manifest = record.xpath('//dcterms:hasFormat').text.split('/').last == 'manifest'
 
   collections = record.xpath('//dcterms:isPartOf')
   in_collection = collections.any?{ |c| c.text == "Cicognara Collection"}
 
-  # puts "incollection: |#{collections}|" if in_collection
   FileUtils.cp file, outdir if has_ids and in_collection
 end
